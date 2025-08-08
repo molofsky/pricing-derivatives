061a4d419f8009962e231728356abd47b10c9ee9
molofsky <molofsky@stanford.edu>
2025-08-08 10:49:38 -0700

    update with error handling and a neat example for a European option

--- one-period-binomial.cpp
+++ one-period-binomial.cpp
@@ -3,38 +3,40 @@
 *
 * S_0 - S_1(H) = uS_0
 *  |
-*     - S_2(T) = dS_0
-*
-* with d < 1 + r < u
+*     - S_1(T) = dS_0
 *
 *
+* with d < 1 + r < u, i.e. no arbitrage exists between the stock and money market
+*
+* An example input for a European option with a fair strike price: 
+* S(0) = 4, X_0 = 1.20, u = 2, d = 0.5, r = 0.25, K = 5
 *
 * @author Adrian Molofsky
 * @version August 8, 2025
 *
 */
-#include <stdlib.h>
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int exercise_option(int S_1, int K) {
-    return std::max(S_1 - K, 0); // (S_1 - K)^+
+double exercise_option(double S_1, double K) {
+    return max(S_1 - K, 0.0); // (S_1 - K)^+
 }
 
-vector<int> one_period_binomial(int S_0, int u, int d) {
+vector<double> one_period_binomial(double S_0, double u, double d) {
     return {u * S_0, d * S_0};
 }
 
 int main() {
-    int S_0, X_0, delta_0, K, u, d, r, b;
+    double S_0, X_0, delta_0, K, u, d, r, b;
 
-    cout << "Stock price: ";
+    cout << "Stock price at time zero: ";
     cin >> S_0;
 
-    cout << "Initial Wealth: ";
+    cout << "Initial wealth: ";
     cin >> X_0;
 
     cout << "Number of shares: ";
@@ -42,31 +44,39 @@ int main() {
 
     cout << "Up factor: ";
     cin >> u;
+    if (u <= 0) {
+        cerr << "Error: up factor must be greater than zero" << endl;
+    }
 
     cout << "Down factor: ";
     cin >> d;
+    if (d <= 0) {
+        cerr << "Error: down factor must be greater than zero" << endl;
+    }
 
     cout << "Interest rate: ";
     cin >> r;
+    if (!(d < 1 + r && 1 + r < u)) {
+        cerr << "Error: assume no arbitrage between stock and money market, i.e. d < 1 + r < u" << endl;
+    }
 
     cout << "Strike price: ";
     cin >> K;
 
-    vector<int> S_1 = one_period_binomial(S_0, u, d);
-    int option_H = exercise_option(S_1[0], K);
-    int option_T = exercise_option(S_1[1], K);
-    int portfolio_H = delta_0 * S_1[0] + (1 + r) * (X_0 - delta_0 * S_0);
-    int portfolio_T = delta_0 * S_1[1] + (1 + r) * (X_0 - delta_0 * S_0);
+    vector<double> S_1 = one_period_binomial(S_0, u, d);
+    double option_H = exercise_option(S_1[0], K);
+    double option_T = exercise_option(S_1[1], K);
+    double portfolio_H = delta_0 * S_1[0] + (1 + r) * (X_0 - delta_0 * S_0);
+    double portfolio_T = delta_0 * S_1[1] + (1 + r) * (X_0 - delta_0 * S_0);
 
-    cout << "/n If heads, value of option" << option_H << "value of portfolio" << portfolio_H << endl;
-    cout << "If tails, value of option" << option_T << "value of portfolio" << portfolio_T << endl;
+    cout << "If heads, stock price: " << S_1[0] << ", value of option: " << option_H << ", value of portfolio: " << portfolio_H << endl;
+    cout << "If tails, stock price: " << S_1[1] << ", value of option: " << option_T << ", value of portfolio: " << portfolio_T << endl;
 
-    if (d < 1 + r < u && payoff_H == portfolio_H && payoff_T == portfolio_T) {
-        cout << "Fair priced option" << endl;
+    if (option_H == portfolio_H && option_T == portfolio_T) {
+        cout << "Fair price of option at time zero" << endl;
     } else {
-        cout << "Arbitrage price at time zero" << endl;
+        cout << "Arbitrage price exists at time zero" << endl;
     }
 
     return 0;
-
 }
